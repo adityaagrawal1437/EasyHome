8c9ee50aa486f5c9b9cecf4c156c23c49e3c0836
Aditya Agrawal <2021ugee043@nitjsr.ac.in>
2024-02-15 17:30:27 +0530

    complete header search form functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -2,8 +2,17 @@ import User from "../models/user.model.js";
 import { errorHandler } from "../utils/error.js";
 import bcryptjs from "bcryptjs";
 import Listing from "../models/listing.model.js";
-export const getUser = (req, res) => {
-  res.send("Signup Api is done");
+export const getUser =async (req, res,next) => {
+  try {
+    const  user = await User.findById(req.params.id);
+    if(!user){
+      return next(errorHandler(404,'User not found'));
+    }
+    const {password:pass , ...rest} = user._doc;
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
 };
 export const updateUser = async (req, res, next) => {
   if (req.user.id !== req.params.id)
